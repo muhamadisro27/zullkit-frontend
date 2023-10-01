c1f7766b3c0c57c2fbff58b858635687e0b4e1a2
muhamadisro27 <isro.sabanur@trans-cosmos@co.id>
2023-10-01 17:50:13 +0700

    feat: add category view & category details view

---
+++ src/views/CategoryDetailView.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from "vue";
+import CardItem from "@/components/ui/homepage/CardItem.vue";
+import { onMounted } from "vue";
+import axios from "axios";
+import { useRoute } from "vue-router";
+
+const category = ref("");
+const items = ref([]);
+const route = useRoute();
+
+const getData = () => {
+  axios
+    .get(
+      `https://zullkit-backend.demo.belajarkoding.com/api/categories?id=${route.params.id}&show_products=1`
+    )
+    .then((res) => {
+      if (res.status == 200) {
+        items.value = res.data.data.products;
+        category.value = res.data.data.name;
+      }
+    })
+    .catch((err) => console.log(err));
+};
+onMounted(() => {
+  getData();
+});
+</script>
+
+<template>
+  <CardItem
+    route_name="product"
+    :title="category"
+    width="lg:w-1/3"
+    :items="items"
+  />
+</template>
