6cf7a4fffc602c692673c23a57790954a3adaac9
muhamadisro27 <isro.sabanur@trans-cosmos@co.id>
2023-10-04 20:33:26 +0700

    feat: add axios register

--- src/components/authentication/RegisterForm.vue
+++ src/components/authentication/RegisterForm.vue
@@ -1,21 +1,39 @@
 <script setup>
-import { reactive } from 'vue';
-import { RouterLink } from 'vue-router';
+import { reactive } from "vue";
+import axios from "axios";
+import { RouterLink } from "vue-router";
 
 const form = reactive({
-  name : '',
-  email : '',
-  password : '',
-})
+  name: "",
+  email: "",
+  password: "",
+  title: "Designer",
+});
 
-const submit = () => {
-  console.log(first)
-}
-
+const submit = async () => {
+  try {
+    await axios
+      .post("https://zullkit-backend.demo.belajarkoding.com/api/register", {
+        name: form.name,
+        email: form.email,
+        password: form.password,
+        title: form.title,
+      })
+      .then((res) => {
+        localStorage.setItem("access_token", res.data.data.access_token);
+        localStorage.setItem("token_type", res.data.data.token_type);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  } catch (error) {
+    console.error("error" + error.message);
+  }
+};
 </script>
 
 <template>
-  <form @submit.prevent="submit">
+  <form>
     <div class="mb-4">
       <label class="block mb-1" for="name">Name</label>
       <input
@@ -39,12 +57,14 @@ const submit = () => {
       <input
         placeholder="Type your password"
         v-model="form.password"
+        @keyup.enter="submit"
         type="password"
         class="block w-full py-3 mt-2 border border-gray-300 rounded-full shadow-sm px-7 focus:border-indigo-300 focus:outline-none focus:ring focus:ring-indigo-200 focus:ring-opacity-50 disabled:bg-gray-100"
       />
     </div>
     <div class="mt-6">
       <button
+        @click.prevent="submit"
         type="button"
         class="inline-flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-full hover:bg-indigo-700 md:py-2 md:text-lg md:px-10 hover:shadow"
       >
