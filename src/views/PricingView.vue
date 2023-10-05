a90d7a3302c88c21baebf67a5c47027426c9e155
muhamadisro27 <isro.sabanur@trans-cosmos@co.id>
2023-10-05 20:15:19 +0700

    feat: payment gateway midtrans

--- src/views/PricingView.vue
+++ src/views/PricingView.vue
@@ -1,4 +1,30 @@
-<script setup></script>
+<script setup>
+import axios from "axios";
+import { RouterLink } from "vue-router";
+
+async function checkout(price) {
+  try {
+    const response = await axios.post(
+      "https://zullkit-backend.demo.belajarkoding.com/api/checkout",
+      {
+        payment_total: price,
+        payment_status: "PENDING",
+      },
+      {
+        headers: {
+          Authorization: `${localStorage.getItem(
+            "token_type"
+          )} ${localStorage.getItem("access_token")}`,
+        },
+      },
+    );
+
+    window.location.href = response.data.data.payment_url;
+  } catch (error) {
+    console.log(error);
+  }
+}
+</script>
 
 <template>
   <main>
@@ -68,12 +94,12 @@
                       Pre-built design screen
                     </li>
                   </ul>
-                  <RouterLink
-                    to="/success"
+                  <button
+                    @click.prevent="checkout(2000)"
                     class="inline-flex items-center justify-center w-full px-8 py-3 text-base font-medium text-black bg-gray-200 border border-transparent rounded-full hover:bg-gray-300 md:py-2 md:text-md md:px-10 hover:shadow"
                   >
                     Checkout Now
-                  </RouterLink>
+                  </button>
                 </div>
               </div>
               <div>
@@ -158,12 +184,12 @@
                       Unlock cloning app
                     </li>
                   </ul>
-                  <RouterLink
-                    to="/success"
+                  <button
+                    @click.prevent="checkout(9000)"
                     class="inline-flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-full hover:bg-indigo-700 md:py-2 md:text-md md:px-10 hover:shadow"
                   >
                     Checkout Now
-                  </RouterLink>
+                  </button>
                 </div>
               </div>
             </div>
